6a0950a81c7dd2f86c9cf621adcaf55dd0440a9f
ShadowMasterX5 <gamemanxx@gmail.com>
2022-11-23 13:41:42 -0500

    11111
    000000

--- source/Engine/Media/Includes/AVFormat.h
+++
@@ -1,6 +0,0 @@
-#ifdef USING_LIBAV
-extern "C" {
-    #include <libavformat/avformat.h>
-    #include <libavformat/avio.h>
-}
-#endif
--- source/Engine/Platforms/MacOS/Filesystem.h
+++
@@ -1,6 +0,0 @@
-#ifndef ENGINE_IO_MACOS_FILESYSTEM
-#define ENGINE_IO_MACOS_FILESYSTEM
-
-extern int MacOS_GetApplicationSupportDirectory(char* buffer, int maxSize);
-
-#endif /* ENGINE_IO_MACOS_FILESYSTEM */
